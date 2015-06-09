e3599f018baeb2a85162c39dc4a8798667dc88b0
shyamrag <shyamrag87@gmail.com>
2015-06-09 10:08:32 +0530

    Create Constants.java

---
+++ src/com/denver/airport/entities/Constants.java
@@ -0,0 +1,12 @@
+package com.denver.airport.entities;
+
+
+public class Constants {
+
+	public static String SECTION= "# Section";
+	public static String CONVEYOR_SYSTEM= "Conveyor System";
+	public static String DEPARTURES= "Departures";
+	public static String BAGS= "Bags";
+	public static String ARRIVAL= "ARRIVAL";
+	public static String BAGGAGE_CLAIM = "BaggageClaim"; 
+}
